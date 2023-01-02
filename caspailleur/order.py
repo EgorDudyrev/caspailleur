b09274f5792ccd5998390060b61844bd110bd229
Egor Dudyrev <egor.dudyrev@yandex.ru>
2023-01-02 14:58:44 +0100

    Add tests for orderings

---
+++ caspailleur/order.py
@@ -0,0 +1,107 @@
+from typing import Dict, List, Tuple, FrozenSet
+from bitarray import frozenbitarray as fbarray
+
+from tqdm import tqdm
+
+
+def topological_sorting(elements: List[fbarray]) -> Tuple[List[fbarray], List[int]]:
+    """Sort the elements so that the first element is the top, the last is the bottom.
+
+    The second output is the mapping from the original indices to the sorted ones
+    """
+    ars_topsort = sorted(elements, key=lambda el: (-el.count(), list(el.itersearch(1))))
+
+    bitars_idx_map = {el: i for i, el in enumerate(ars_topsort)}
+    orig_to_topsort_indices_map = [bitars_idx_map[el] for el in elements]
+    return ars_topsort, orig_to_topsort_indices_map
+
+
+def construct_spanning_tree(elements: List[fbarray]) -> List[int]:
+    """Find a parent for each element. Elements should be topologically presorted"""
+    parents = [None] * len(elements)
+    for i, el in enumerate(elements[1:], start=1):
+        for j in range(i-1, 0-1, -1):
+            if el & elements[j] == el:
+                parents[i] = j
+                break
+    return parents
+
+
+def split_sptree_to_chains(span_tree_parents: List[int]) -> List[List[int]]:
+    span_tree_parents_set = set(span_tree_parents)
+    leafs = [i for i in range(len(span_tree_parents)) if i not in span_tree_parents_set]
+    del span_tree_parents_set
+
+    chains = []
+    for leaf in leafs:
+        node, chain = leaf, []
+        while node is not None:
+            chain.append(node)
+            node = span_tree_parents[node]
+        chains.append(chain[::-1])
+    return chains
+
+
+def find_parents_in_chains(chains: List[List[int]], elements: List[fbarray], use_tqdm: bool = False)\
+        -> List[FrozenSet[int]]:
+    parents_list = []
+    for el in tqdm(elements, disable=not use_tqdm, desc='Iterating elements'):
+        if el.all():
+            parents_list.append(frozenset([]))
+            continue
+
+        parents = []
+        for chain in chains:
+            for other_i in chain:
+                if elements[other_i] == el:
+                    break
+                if elements[other_i] & el != el:
+                    break
+
+                parent = other_i
+            parents.append(parent)
+        parents_list.append(frozenset(parents))
+    return parents_list
+
+
+def drop_transitive_parents(parents_list: List[FrozenSet[int]]) -> List[FrozenSet[int]]:
+    ancestors_list = [set(parents) for parents in parents_list]
+    for el_i, parents in enumerate(parents_list[1:], start=1):
+        for parent_i in parents:
+            ancestors_list[el_i] |= ancestors_list[parent_i]
+
+    new_parents_list = [set(parents) for parents in parents_list]
+    for el_i, parents in enumerate(parents_list[1:], start=1):
+        for parent_i in sorted(parents, reverse=True):
+            if parent_i in new_parents_list[el_i]:
+                new_parents_list[el_i] -= ancestors_list[parent_i]
+
+    new_parents_list = [frozenset(parents) for parents in new_parents_list]
+    return new_parents_list
+
+
+def sort_ba_inclusion(bitarrays: List[fbarray], use_tqdm: bool = False) -> List[FrozenSet[int]]:
+    """Take the list of bitarrays and output the parents dict (succeeding elements by consumption order)"""
+    bitars_topsort, orig_topsort_imap = topological_sorting(bitarrays)
+    span_tree_parents = construct_spanning_tree(bitars_topsort)
+    chains_list = split_sptree_to_chains(span_tree_parents)
+    parents_list_trans = find_parents_in_chains(chains_list, bitars_topsort, use_tqdm=use_tqdm)
+    parents_list_final = drop_transitive_parents(parents_list_trans)
+
+    topsort_orig_imap = {v: i for i, v in enumerate(orig_topsort_imap)}
+    parents_orig = [frozenset(topsort_orig_imap[parent] for parent in parents_list_final[topsort_i])
+                    for topsort_i in orig_topsort_imap]
+    return parents_orig
+
+
+def inverse_order(order: List[FrozenSet[int]]) -> List[FrozenSet[int]]:
+    inversed = [list() for _ in order]
+    for child, parents in enumerate(order):
+        for parent in parents:
+            inversed[parent].append(child)
+    inversed = [frozenset(vs) for vs in inversed]
+    return inversed
+
+
+def sort_ba_subsumption(bitarrays: List[fbarray], use_tqdm: bool = False) -> List[FrozenSet[int]]:
+    return inverse_order(sort_ba_inclusion(bitarrays, use_tqdm))
---
+++ tests/test_order.py
@@ -0,0 +1,75 @@
+from caspailleur.base_functions import iset2ba
+from caspailleur import order
+
+
+def test_topological_sorting():
+    elements = [iset2ba(iset, 3) for iset in [[0, 1, 2], [0, 1], [1], [2], []]]
+    assert order.topological_sorting(elements) == (elements, [0, 1, 2, 3, 4])
+
+    idxs_unordered = [1, 0, 3, 2, 4]
+    elements_unordered = [elements[i] for i in idxs_unordered]
+    orig_to_topsort_map_true = [idxs_unordered.index(i) for i in range(len(idxs_unordered))]
+    assert order.topological_sorting(elements_unordered) == (elements, orig_to_topsort_map_true)
+
+
+def test_construct_spanning_tree():
+    elements = [iset2ba(iset, 3) for iset in [[0, 1, 2], [0, 1], [1], [2], []]]
+    sptree_true = [None, 0, 1, 0, 3]
+    sptree = order.construct_spanning_tree(elements)
+    assert sptree == sptree_true
+
+
+def test_split_sptree_to_chains():
+    # elements = [iset2ba(iset, 3) for iset in [[0, 1, 2], [0, 1], [1], [2], []]]
+    sptree = [None, 0, 1, 0, 3]
+    chains_true = [[0, 1, 2], [0, 3, 4]]
+
+    chains = order.split_sptree_to_chains(sptree)
+    assert chains == chains_true
+
+
+def test_find_parents_in_chains():
+    elements = [iset2ba(iset, 3) for iset in [[0, 1, 2], [0, 1], [1], [2], []]]
+    chains = [[0, 1, 2], [0, 3, 4]]
+    parents_per_chains_true = [[], [0, 0], [1, 0], [0, 0], [2, 3]]  # for now, we include the transitive parents
+    parents_true = [set(parents) for parents in parents_per_chains_true]
+
+    parents_list = order.find_parents_in_chains(chains, elements, use_tqdm=False)
+    assert parents_list == parents_true
+
+
+def test_drop_transitive_parents():
+    parents_per_chains = [set(), {0}, {0, 1}, {0}, {2, 3}]
+    parents_nontrans_true = [set(), {0}, {1}, {0}, {2, 3}]
+
+    parents_nontrans = order.drop_transitive_parents(parents_per_chains)
+    assert parents_nontrans == parents_nontrans_true
+
+
+def test_sort_ba_inclusion():
+    elements = [iset2ba(iset, 3) for iset in [[0, 1, 2], [0, 1], [1], [2], []]]
+    inclusion_order_true = [set(), {0}, {1}, {0}, {2, 3}]
+    inclusion_order = order.sort_ba_inclusion(elements, use_tqdm=False)
+    assert inclusion_order == inclusion_order_true
+
+    elements_unordered = [[0, 1], [0, 1, 2], [2], [1], []]
+    elements_unordered = [iset2ba(iset, 3) for iset in elements_unordered]
+    order_unordered =    [{1},    set(),     {1}, {0}, {2, 3}]
+    assert order.sort_ba_inclusion(elements_unordered) == order_unordered
+
+
+def test_inverse_order():
+    inclusion_order = [set(), {0}, {1}, {0}, {2, 3}]
+    subsumption_order_true = [{1, 3}, {2}, {4}, {4}, set()]
+    assert order.inverse_order(inclusion_order) == subsumption_order_true
+    assert order.inverse_order(order.inverse_order(inclusion_order)) == inclusion_order
+
+
+def test_sort_ba_subsumption():
+    elements = [iset2ba(iset, 3) for iset in [[0, 1, 2], [0, 1], [1], [2], []]]
+    subsumption_order_true = [{1, 3}, {2}, {4}, {4}, set()]
+    assert order.sort_ba_subsumption(elements) == subsumption_order_true
+
+    elements_unordered = [iset2ba(iset, 3) for iset in [[0, 1], [0, 1, 2], [2], [1], []]]
+    order_unordered = [{3}, {0, 2}, {4}, {4}, set()]
+    assert order.sort_ba_subsumption(elements_unordered) == order_unordered
