92a3689e303c55b8caaa0b68ed0fe12c0790ba43
Egor Dudyrev <egor.dudyrev@yandex.ru>
2023-01-19 14:41:26 +0100

    Fasten up linearity index function

--- caspailleur/indices.py
+++ caspailleur/indices.py
@@ -3,22 +3,26 @@ from bitarray import frozenbitarray as fbarray
 
 
 def linearity_index(parents: List[FrozenSet[int]], include_top_bottom: bool = True) -> float:
-    parents_trans = {k: set(vs) for k, vs in enumerate(parents)}
-    for child in sorted(parents_trans):
-        for parent in list(parents_trans[child]):
-            parents_trans[child] |= parents_trans[parent]
+    assert all([max(parents_) < i for i, parents_ in enumerate(parents) if parents_]),\
+        "`parents` relation should be defined on a list, topologically sorted by descending order." \
+        " So all parents of element `i` should have smaller indices"
 
-    n_nodes = len(parents_trans)
-    if include_top_bottom:
-        indices_to_iterate = ((i, j) for i in range(0, n_nodes) for j in range(i+1, n_nodes))
-    else:
-        indices_to_iterate = ((i, j) for i in range(1, n_nodes - 1) for j in range(i+1, n_nodes - 1))
+    n_elems = len(parents)
+    trans_parents = [set(parents_) for parents_ in parents]
+    for i, parents_ in enumerate(parents):
+        for parent in parents_:
+            trans_parents[i] |= trans_parents[parent]
+    assert len(trans_parents[0]) == 0 and len(trans_parents[-1]) == n_elems - 1, \
+        "Given `parents` relation should represent the lattice (i.e. have only one top and one bottom)"
 
-    n_comparable = 0
-    n_pairs = 0
-    for i, j in indices_to_iterate:
-        n_comparable += int(i in parents_trans[j])
-        n_pairs += 1
+    n_comparable = sum(len(tparents) for tparents in trans_parents)
+    n_pairs = n_elems * (n_elems - 1) // 2
+
+    if not include_top_bottom:
+        # for (n_elems - 2) elements between top and bottom, drop pairs with the top,
+        # for bottom element drop all (n_elems - 1) relations
+        n_comparable -= (n_elems - 2) + (n_elems - 1)
+        n_pairs -= 2*(n_elems-1) - 1
 
     return n_comparable / n_pairs
 
