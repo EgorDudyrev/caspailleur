21a049e9a622e9c96000f120734ab7354b8433fb
Egor Dudyrev <egor.dudyrev@yandex.ru>
2023-11-17 16:08:22 +0100

    Fix the bug with ordering of not-lattices

--- caspailleur/order.py
+++ caspailleur/order.py
@@ -75,6 +75,8 @@ def sort_intents_inclusion(intents: List[fbarray], use_tqdm=False, return_transi
         children = bitarray(zero_intents)
         for new_m in (all_attrs & ~intent).itersearch(True):
             meet_idx = (common_descendants & attrs_descendants[new_m]).find(True)
+            if meet_idx == -1:  # no meet found
+                continue
             children[meet_idx] = True
 
         trans_children = bitarray(zero_intents)
