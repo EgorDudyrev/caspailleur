fc23f9716ae9a1274b3efd62d25a407430854ef1
Egor Dudyrev <egor.dudyrev@loria.fr>
2024-05-10 15:19:18 +0200

    Refactored mine_descriptions function

--- caspailleur/api.py
+++ caspailleur/api.py
@@ -3,11 +3,12 @@ from typing import Iterator
 import pandas as pd
 from bitarray import frozenbitarray as fbarray
 
+import io
 from .base_functions import powerset, extension
-from .io import ContextType, to_bitarrays, transpose_context
+from .io import ContextType, to_bitarrays, transpose_context, isets2bas
 from . import indices as idxs
 from . import definitions
-
+from . import mine_equivalence_classes as mec, implication_bases as ibases
 
 def iter_descriptions(data: ContextType) -> Iterator[dict]:
     bitarrays, objects, attributes = to_bitarrays(data)
@@ -20,7 +21,7 @@ def iter_descriptions(data: ContextType) -> Iterator[dict]:
         stats = {
             'description': {attributes[attr_i] for attr_i in description_idxs},
             'extent': {objects[obj_i] for obj_i in extent_ba.itersearch(True)},
-            'closure': {attributes[attr_i] for attr_i in definitions.closure(description_idxs, attr_extents)},
+            'intent': {attributes[attr_i] for attr_i in definitions.closure(description_idxs, attr_extents)},
             'support': idxs.support_by_description(description_idxs, attr_extents, extent_ba),
             'delta-stability': idxs.delta_stability_by_description(description_idxs, attr_extents),
             'is_closed': definitions.is_closed(description_idxs, attr_extents),
@@ -35,7 +36,36 @@ def iter_descriptions(data: ContextType) -> Iterator[dict]:
 
 
 def mine_descriptions(data: ContextType) -> pd.DataFrame:
-    return pd.DataFrame(iter_descriptions(data))
+    bitarrays, objects, attributes = to_bitarrays(data)
+    attr_extents = transpose_context(bitarrays)
+
+    intents_ba = mec.list_intents_via_LCM(bitarrays)
+    extents_ba = [fbarray(extension(intent, attr_extents)) for intent in intents_ba]
+    ext_int_map = dict(zip(extents_ba, intents_ba))
+    keys_ba = mec.list_keys(intents_ba)
+    passkeys_ba = mec.list_passkeys(intents_ba)
+    ppremises_ba = dict(ibases.iter_proper_premises_via_keys(intents_ba, keys_ba))
+    pintents_ba = dict(ibases.list_pseudo_intents_via_keys(ppremises_ba.items(), intents_ba))
+
+    descr_df = pd.DataFrame(index=pd.Series(list(map(set, powerset(range(len(attributes))))), name='description_idxs'))
+    descr_df['description'] = [{attributes[m_i] for m_i in descr_idxs} for descr_idxs in descr_df.index]
+    descr_df['extent_ba'] = [extension(descr, attr_extents) for descr in descr_df.index]
+    descr_df['extent'] = [{objects[g_i] for g_i in extent_ba.itersearch(True)} for extent_ba in descr_df['extent_ba']]
+    descr_df['intent'] = [{attributes[m_i] for m_i in ext_int_map[extent_ba].itersearch(True)}
+                           for extent_ba in descr_df['extent_ba']]
+    descr_df['support'] = [idxs.support_by_description(..., attr_extents, extent)
+                           for descr, extent in descr_df['extent_ba'].items()]
+    descr_df['delta-stability'] = [idxs.delta_stability_by_description(descr, attr_extents, extent)
+                                   for descr, extent in descr_df['extent_ba'].items()]
+    descr_df['is_closed'] = descr_df['intent'] == descr_df['description']
+    descr_df['descr_ba'] = list(isets2bas(descr_df.index, len(attributes)))
+    descr_df['is_key'] = [descr_ba in keys_ba for descr_ba in descr_df['descr_ba']]
+    descr_df['is_passkey'] = [descr_ba in passkeys_ba for descr_ba in descr_df['descr_ba']]
+    descr_df['is_proper_premise'] = [descr_ba in ppremises_ba for descr_ba in descr_df['descr_ba']]
+    descr_df['is_pseudo_intent'] = [descr_ba in pintents_ba for descr_ba in descr_df['descr_ba']]
+
+    descr_df = descr_df.drop(columns=['extent_ba', 'descr_ba']).reset_index(drop=True)
+    return descr_df
 
 
 def mine_concepts(data: ContextType) -> pd.DataFrame:
--- tests/test_api.py
+++ tests/test_api.py
@@ -3,55 +3,13 @@ import pandas as pd
 from caspailleur import api
 
 
-def test_iter_descriptions():
-    data = {'g1': ['a', 'b'], 'g2': ['b', 'c']}
-
-    descriptions_data_true = [
-        {
-            'description': set(), 'extent': {'g1', 'g2'}, 'closure': {'b'},
-            'support': 2, 'delta-stability': 0,
-            'is_closed': False, 'is_key': True, 'is_passkey': True, 'is_proper_premise': True, 'is_pseudo_intent': True
-        }, {
-            'description': {'a'}, 'extent': {'g1'}, 'closure': {'a', 'b'},
-            'support': 1, 'delta-stability': 0,
-            'is_closed': False, 'is_key': True, 'is_passkey': True, 'is_proper_premise': False, 'is_pseudo_intent': False
-        }, {
-            'description': {'b'}, 'extent': {'g1', 'g2'}, 'closure': {'b'},
-            'support': 2, 'delta-stability': 1,
-            'is_closed': True, 'is_key': False, 'is_passkey': False, 'is_proper_premise': False, 'is_pseudo_intent': False
-        }, {
-            'description': {'c'}, 'extent': {'g2'}, 'closure': {'b', 'c'},
-            'support': 1, 'delta-stability': 0,
-            'is_closed': False, 'is_key': True, 'is_passkey': True, 'is_proper_premise': False, 'is_pseudo_intent': False
-        }, {
-            'description': {'a', 'b'}, 'extent': {'g1'}, 'closure': {'a', 'b'},
-            'support': 1, 'delta-stability': 1,
-            'is_closed': True, 'is_key': False, 'is_passkey': False, 'is_proper_premise': False, 'is_pseudo_intent': False
-        }, {
-            'description': {'a', 'c'}, 'extent': set(), 'closure': {'a', 'b', 'c'},
-            'support': 0, 'delta-stability': 0,
-            'is_closed': False, 'is_key': True, 'is_passkey': True, 'is_proper_premise': False, 'is_pseudo_intent': False
-        }, {
-            'description': {'b', 'c'}, 'extent': {'g2'}, 'closure': {'b', 'c'},
-            'support': 1, 'delta-stability': 1,
-            'is_closed': True, 'is_key': False, 'is_passkey': False, 'is_proper_premise': False, 'is_pseudo_intent': False
-        }, {
-            'description': {'a', 'b', 'c'}, 'extent': set(), 'closure': {'a', 'b', 'c'},
-            'support': 0, 'delta-stability': 0,
-            'is_closed': True, 'is_key': False, 'is_passkey': False, 'is_proper_premise': False, 'is_pseudo_intent': False
-        },
-    ]
-    descriptions_data = list(api.iter_descriptions(data))
-    assert descriptions_data == descriptions_data_true
-
-
 def test_mine_descriptions():
     data = {'g1': ['a', 'b'], 'g2': ['b', 'c']}
 
     descriptions_data_true = pd.DataFrame({
         'description': [set(), {'a'}, {'b'}, {'c'}, {'a', 'b'}, {'a', 'c'}, {'b', 'c'}, {'a', 'b', 'c'}],
         'extent': [{'g1', 'g2'}, {'g1'}, {'g1', 'g2'}, {'g2'}, {'g1'}, set(), {'g2'}, set()],
-        'closure': [{'b'}, {'a', 'b'}, {'b'}, {'b', 'c'}, {'a', 'b'}, {'a', 'b', 'c'}, {'b', 'c'}, {'a', 'b', 'c'}],
+        'intent': [{'b'}, {'a', 'b'}, {'b'}, {'b', 'c'}, {'a', 'b'}, {'a', 'b', 'c'}, {'b', 'c'}, {'a', 'b', 'c'}],
         'support': [2, 1, 2, 1, 1, 0, 1, 0],
         'delta-stability': [0, 0, 1, 0, 1, 0, 1, 0],
         'is_closed': [False, False, True, False, True, False, True, True],
@@ -63,4 +21,12 @@ def test_mine_descriptions():
     descriptions_data = api.mine_descriptions(data)
     assert list(descriptions_data.index) == list(descriptions_data_true.index)
     assert list(descriptions_data.columns) == list(descriptions_data_true.columns)
-    assert (descriptions_data == descriptions_data_true).all(None)
+    for f in descriptions_data:
+        assert (descriptions_data[f] == descriptions_data_true[f]).all(), f"Problematic column {f}"
+
+
+def test_iter_descriptions():
+    data = {'g1': ['a', 'b'], 'g2': ['b', 'c']}
+
+    descriptions_data = list(api.iter_descriptions(data))
+    assert (pd.DataFrame(descriptions_data) == api.mine_descriptions(data)).all(None)
